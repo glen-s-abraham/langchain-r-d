3a6eaaa7f4521b5db1d2794e1747188ce742b956
glen-s-abraham <glenprojects666@gmail.com>
2023-08-19 12:38:43 +0530

    first contacts with gpt

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import os
 from  dotenv import load_dotenv,find_dotenv
+from langchain.llms import OpenAI
 load_dotenv(find_dotenv(),override=True)
 
-print(os.environ.items())
+gpt_client = OpenAI(model='text-davinci-003',temperature=0.7,max_tokens=512,openai_api_key=os.environ.get("OPEN_API_KEY"),max_retries=1)
+
+questions = [
+    "... is the capital of india",
+    "Generate an original tagline for a burger restaurent",
+    "What is the formula to calculate a line angle"
+]
+
+answers = gpt_client.generate(questions);
+
+for answer in answers:
+    print(answer)
